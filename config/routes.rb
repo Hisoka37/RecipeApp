c4074181e9417f7d2b35338e027cc7a3c080cbc1
Walid Kabboussa <walidkabou@gmail.com>
2023-07-04 20:56:58 +0100

    Setup shopping list and add inrgredient methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :recipes, except: [:edit] do
-    resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
+  resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
   end
   
   resources :foods
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   
   # Add the route for the edit action
   get 'recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-
+  get '/general_shopping_list' , to: 'users#shopping_list'
+  get 'recipes/:recipe_id/new_ingredient' , to: 'recipes#new_ingredient' , as: :new_ingredient
+  post 'recipes/:recipe_id/new_ingredient' , to: 'recipes#create_ingredient' , as: :create_ingredient
   root "foods#index"
 end
