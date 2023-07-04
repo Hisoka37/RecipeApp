06c4f726b875b702962351b752ce80fad060dc8c
Walid Kabboussa <walidkabou@gmail.com>
2023-07-04 18:53:42 +0100

    Setup recipe list and run test for recipes controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :recipes, only: [:new, :create, :index, :show, :destroy, :put] do
+  
+  resources :recipes, except: [:edit] do
     resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
   end
+  
   resources :foods
   resources :users
+  
+  # Add the route for the edit action
+  get 'recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
 
   root "foods#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
-
-
