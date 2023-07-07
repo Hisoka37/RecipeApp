2c041220dd2af22d9a3eaa4c39ad5f21c9b8e58b
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-07-07 09:20:12 +0100

    make change

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.includes(:user).where(public: true).order(created_at: :desc)
-    @users = User.includes(:recipes).where(recipes: { public: true })
   end
 end
